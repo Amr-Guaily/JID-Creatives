ff8e4edbce3df301631b52a181c6595aec82c843
Amr-Guaily <amrguaily@gmail.com>
2025-07-21 19:29:42 +0300

    Implement movie details page
    - Added MovieDetailsPage to fetch and display movie details.
    - Updated RootLayout with a new header for the Movie Search App.
    - Improved Home component to use `Suspence`

---
+++ app/[id]/page.tsx
@@ -0,0 +1,27 @@
+import ErrorMessage from '@/components/ErrorMessage';
+import MovieDetails from '@/components/MovieDetails';
+import { MovieService } from '@/lib/movieService';
+import { Movie } from '@/types';
+import { notFound } from 'next/navigation';
+
+export default async function MovieDetailsPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const movieID = (await params).id;
+
+  try {
+    const movieDetails: Movie = await MovieService.getMovieDetails(movieID);
+
+    if (!movieDetails || movieDetails.Response === 'False') {
+      notFound();
+    }
+
+    return <MovieDetails movie={movieDetails} />;
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error ? error.message : 'Failed to load movie details';
+    return <ErrorMessage message={errorMessage} />;
+  }
+}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,34 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com',
+        port: '',
+        pathname: '/images/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ia.media-imdb.com',
+        port: '',
+        pathname: '/images/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'img.omdbapi.com',
+        port: '',
+        pathname: '/**',
+      },
+      // Allow any HTTPS image source as a fallback
+      // You can be more specific based on the domains you encounter
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
